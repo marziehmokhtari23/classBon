608edd0ad7f67cbe27d29a20f4934b6c89b27de8
marzieh mokhtari <marziehmokhtari67@gmail.com>
2024-09-01 08:57:34 +0330

    feat:(badge.tsx-badge.css-badge-types.ts)

--- src/app/(course)/_components/course-card.tsx
+++ src/app/(course)/_components/course-card.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/app/_components/badge";
 import { CourseSummery } from "@/types/course-summer.interface";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,21 +12,24 @@ export const CourseCard: FC<CourseCardProps> = ({ coverImageId, subTitle, level,
                 <Image src={`https://api.classbon.com/api/picture/${coverImageId}`} alt="title" width={550} height={327} />
             </figure>
             <div className="mt-2 flex gap-4 font-semibold dark:text-info px-3 py-2">
-                {recordStatus}
-                {level}
+                <Badge variant="info">{recordStatus}</Badge>
+                <Badge variant="accent">{level}</Badge>
             </div>
             <div className="card-body">
-                <Link href={`/course/${slug}`}  className="card-title">
-                {title}
+                <Link href={`/course/${slug}`} className="card-title">
+                    {title}
                 </Link>
+                <p>{subTitle}</p>
                 <div>
-                    {duration}
+                    <Badge variant="warning">{duration}</Badge>
                     {basePrice}
                 </div>
-                <Link href={`/course/${slug}`} className="card-footer animated-icon justify-center">
-                مشاهده جزءیات دوره
-                </Link>
+
             </div>
+            <Link href={`/course/${slug}`} className="card-footer animated-icon justify-center">
+                مشاهده جزئیات دوره
+            </Link>
         </div>
+
     )
 }
---
+++ src/app/_components/badge/badge-types.ts
@@ -0,0 +1,5 @@
+import { ComponentBase } from "../types/component-base-type";
+import { ReactNode } from "react";
+export type BadgeProps=Omit<ComponentBase,'isDisabled'> & {
+    children:ReactNode
+}
